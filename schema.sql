51a0646d8004403c0521ee0451473e0cfa37230f
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-02-02 14:12:26 -0500

    initial commit

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE apple_ceos (
+    id serial PRIMARY KEY,
+    slug text NOT NULL,
+    name text NOT NULL,
+    year integer
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO apple_ceos (slug, name, year) VALUES 
+('steve_jobs', 'Steve Jobs', 1976), 
+('mike_markkula', 'Mike Markkula', 1977), 
+('mike_scott', 'Mike Scott', 1978), 
+('michael_spindler', 'Michael Spindler', 1993), 
+('gil_amelio', 'Gil Amelio', 1996), 
+('steve_jobs2', 'Steve Jobs', 1997), 
+('tim_cook', 'Tim Cook', 2011);
